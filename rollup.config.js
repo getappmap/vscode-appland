36a760a098db3728f5e43951fa6a221bfe353bcb
Dustin Byrne <dustinsbyrne@gmail.com>
2020-12-18 00:33:32 -0500

    Bundle with rollup
    Rollup will now bundle SCSS into it's own CSS bundle in addition to
    compiling TypeScript.
    
    Typescript compilation is handled by Sucrase:
    https://github.com/alangpierce/sucrase

---
+++ rollup.config.js
@@ -0,0 +1,25 @@
+import { terser } from 'rollup-plugin-terser';
+import resolve from '@rollup/plugin-node-resolve';
+import scss from 'rollup-plugin-scss';
+import sucrase from '@rollup/plugin-sucrase';
+import eslint from '@rollup/plugin-eslint';
+
+export default {
+  input: 'src/extension.ts',
+  output: {
+    file: 'out/extension.js',
+    format: 'cjs'
+  },
+  plugins: [
+    resolve(),
+    scss(),
+    eslint({
+      exclude: 'src/scss/**',
+    }),
+    sucrase({
+      exclude: ["node_modules/**"],
+      transforms: ["typescript"],
+    }),
+    terser(),
+  ],
+};
