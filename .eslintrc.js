67bf72bcce6c6a07416c9a698888eb1ab45d5295
Dustin Byrne <dustinsbyrne@gmail.com>
2021-05-11 17:18:51 -0400

    style: specify and fix naming conventions

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,4 +7,15 @@ module.exports = {
     'plugin:prettier/recommended',
     'plugin:@typescript-eslint/recommended',
   ],
+  rules: {
+    '@typescript-eslint/naming-convention': [
+      'error',
+      { selector: 'variableLike', format: ['camelCase', 'UPPER_CASE'] },
+      {
+        selector: 'variable',
+        modifiers: ['exported'],
+        format: ['PascalCase'],
+      },
+    ],
+  },
 };
