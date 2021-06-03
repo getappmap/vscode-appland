005f04a80e4c873daeb028ad0ffeb1ed0b32d677
Dustin Byrne <dustinsbyrne@gmail.com>
2021-06-03 17:21:51 -0400

    feat: Upgrade to @appland/components, @appland/models

--- types/@appland/index.d.ts
+++ types/@appland/index.d.ts
@@ -1,2 +1,9 @@
-declare module '@appland/models';
-declare module '@appland/diagrams';
+declare module '@appland/models' {
+  class AppMap {}
+  class AppMapBuilder {
+    normalize(): AppMapBuilder;
+    build(): AppMap;
+  }
+
+  function buildAppMap(data: string | Record<string, unknown>): AppMapBuilder;
+}
