b5775ad3228259e89a0e4a400980147a8df194c3
Dustin Byrne <dustinsbyrne@gmail.com>
2021-07-02 15:55:01 -0400

    Improve .gitignore handling

--- src/telemetry/properties/versionControlGit.ts
+++ src/telemetry/properties/versionControlGit.ts
@@ -4,35 +4,71 @@ import { join, dirname } from 'path';
 import globToRegExp from 'glob-to-regexp';
 import VersionControlProperties from './versionControl';
 
-async function getIgnoreGlobs() {
-  const gitIgnoreUris = await vscode.workspace.findFiles('**/.gitignore');
-  return (
-    await Promise.all(
-      gitIgnoreUris.map((uri) => {
-        const rootDir = dirname(uri.fsPath);
-
-        return fs.readFile(uri.fsPath).then((buf) =>
-          buf
-            .toString()
-            .replace(/#.*$/gm, '')
-            .split(/^/gm)
-            .map((line) => line.trim())
-            .filter((line) => line !== '')
-            .map((glob) => join(rootDir, glob))
-        );
-      })
-    )
-  )
-    .flat()
+async function regexFromGitIgnore(filePath: PathLike): Promise<readonly RegExp[]> {
+  const directory = dirname(filePath as string);
+  const content = await fs.readFile(filePath);
+  return content
+    .toString()
+    .replace(/#.*$/gm, '')
+    .split(/^/gm)
+    .map((line) => line.trim().replace(/^\//, `${directory}/`))
+    .filter((line) => line !== '')
     .map((glob) => globToRegExp(glob, { flags: 'g' }));
 }
 
+async function ignoresForWorkspaceFolder(
+  wsFolder: vscode.WorkspaceFolder
+): Promise<readonly RegExp[]> {
+  const directories = [wsFolder.uri.fsPath];
+  const allGitIgnores: RegExp[] = [/\.git/];
+
+  for (;;) {
+    const currentDirectory = directories.pop() as string;
+    if (!currentDirectory) {
+      break;
+    }
+
+    const files = await fs.readdir(currentDirectory, { withFileTypes: true });
+    const gitIgnore = files.find((file) => file.isFile() && file.name === '.gitignore');
+    if (gitIgnore) {
+      const gitIgnores = await regexFromGitIgnore(join(currentDirectory, gitIgnore.name));
+      gitIgnores.forEach((re) => allGitIgnores.push(re));
+    }
+
+    files.filter((file) => {
+      if (!file.isDirectory()) {
+        return false;
+      }
+
+      const fullPath = join(currentDirectory, file.name);
+      const isIgnored = allGitIgnores.some((re) => re.test(fullPath));
+      if (!isIgnored) {
+        directories.push(fullPath);
+      }
+    });
+  }
+
+  return allGitIgnores;
+}
+
+async function getIgnoreGlobs(): Promise<readonly RegExp[]> {
+  const { workspaceFolders } = vscode.workspace;
+  if (!workspaceFolders) {
+    return Promise.resolve([]);
+  }
+
+  const workspaceIgnores = await Promise.all(
+    workspaceFolders.map(async (wsFolder) => await ignoresForWorkspaceFolder(wsFolder))
+  );
+
+  return workspaceIgnores.flat();
+}
+
 export default class GitProperties implements VersionControlProperties {
-  private ignoreRegex: Array<RegExp> | undefined;
+  private ignoreRegex: readonly RegExp[] = [];
 
   public async initialize(): Promise<void> {
     this.ignoreRegex = await getIgnoreGlobs();
-    console.log(this.ignoreRegex);
   }
 
   public isIgnored(path: PathLike): boolean {
@@ -40,7 +76,6 @@ export default class GitProperties implements VersionControlProperties {
       return false;
     }
     const result = this.ignoreRegex.some((re) => re.test(path as string));
-    // console.log(`${path as string} is ${result ? 'ignored' : 'not ignored'}`);
     return result;
   }
 }
