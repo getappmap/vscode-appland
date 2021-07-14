71dc917c047fed91c18cb10cc397b8c9d07c83ed
Alan Potter <alanjpotter@gmail.com>
2021-07-14 05:22:47 -0400

    fix lint problems

--- src/agent/appMapAgentPython.ts
+++ src/agent/appMapAgentPython.ts
@@ -1,89 +1,100 @@
-import { PathLike } from "fs";
-import { isNull } from "util";
-import { exec, AgentExecOptions, AgentExecReturn } from "../util";
-import AppMapAgent, { AppMapAgentBase, FilesResponse, InitResponse, InstallResult, StatusResponse } from "./appMapAgent";
+import { PathLike } from 'fs';
+import { exec, AgentExecOptions, AgentExecReturn } from '../util';
+import AppMapAgent, {
+  AppMapAgentBase,
+  FilesResponse,
+  InitResponse,
+  InstallResult,
+  StatusResponse,
+} from './appMapAgent';
 
 export default class AppMapAgentPython extends AppMapAgentBase implements AppMapAgent {
-    readonly language = 'python';
+  readonly language = 'python';
 
-    private async usesPoetry(path: PathLike): Promise<boolean> {
-        const process = await exec('poetry', ['check'], { cwd: path as string });
-        return process.exitCode === 0;
+  private async usesPoetry(path: PathLike): Promise<boolean> {
+    const process = await exec('poetry', ['check'], { cwd: path as string });
+    return process.exitCode === 0;
+  }
+
+  private async projectExec(
+    path: PathLike,
+    cmd: string,
+    args: ReadonlyArray<string> | null | undefined,
+    options?: AgentExecOptions | null | undefined
+  ): Promise<AgentExecReturn> {
+    if (this.usesPoetry(path)) {
+      let newArgs = ['run', cmd];
+      if (args !== undefined && args !== null) {
+        newArgs = newArgs.concat(args);
+      }
+      cmd = 'poetry';
+      args = newArgs;
+      options = options ?? {};
+      options.cwd = path as string;
+      options.env = {
+        ...process.env,
+        ...options.env,
+        APPMAP_AGENT_POETRY: 'true',
+        APPMAP_LOG_LEVEL: 'info',
+      };
+    }
+    return exec(cmd, args, options);
+  }
+
+  async isInstalled(path: PathLike): Promise<boolean> {
+    const process = await this.projectExec(path, 'pip', ['show', 'appmap']);
+    return process.exitCode === 0;
+  }
+
+  async install(path: PathLike): Promise<InstallResult> {
+    if (this.usesPoetry(path)) {
+      const installed = await this.isInstalled(path);
+      const cmd = installed ? ['update'] : ['add', '--dev'];
+      console.log(`install, cmd ${cmd}`);
+      const pkg = '../../applandinc/appmap-python/dist/appmap-0.0.0-py3-none-any.whl';
+      const { stderr, exitCode } = await exec('poetry', cmd.concat([pkg]), {
+        cwd: path as string,
+        output: true,
+      });
+      if (exitCode !== 0) {
+        throw Error(stderr);
+      }
+      return 'installed';
+    }
+    throw new Error('Method not implemented.');
+  }
+
+  async init(path: PathLike): Promise<InitResponse> {
+    const { stdout, stderr, exitCode } = await this.projectExec(path, 'appmap-agent-init', [], {
+      cwd: path as string,
+    });
+    console.log(`init ${path}`);
+
+    if (exitCode !== 0) {
+      console.log(`init failed, ${stderr}`);
+      throw new Error(stderr);
     }
 
-    private async projectExec(path: PathLike, 
-        cmd: string, 
-        args: ReadonlyArray<string> | null | undefined, 
-        options?: AgentExecOptions | null | undefined
-        ): Promise<AgentExecReturn> {
-        if (this.usesPoetry(path)) {
-            let newArgs = ['run', cmd]
-            if (args !== undefined && args !== null) {
-                newArgs = newArgs.concat(args);
-            }
-            cmd = 'poetry';
-            args = newArgs;
-            options = options ?? {};
-            options.cwd = path as string;
-            options.env = {
-                ...process.env,
-                ...options.env,
-                'APPMAP_LOG_LEVEL': 'info'
-            };
-        }
-        return exec(cmd, args, options);
-    }
+    return this.writeConfig(path, stdout);
+  }
 
-    async isInstalled(path: PathLike): Promise<boolean> {
-        const process = await this.projectExec(path, 'pip', ['show', 'appmap']);
-        return process.exitCode === 0;
-    }
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  files(path: PathLike): Promise<FilesResponse> {
+    throw new Error('Method not implemented.');
+  }
+  async status(path: PathLike): Promise<StatusResponse> {
+    if (this.usesPoetry(path)) {
+      const { stdout, stderr, exitCode } = await this.projectExec(path, 'appmap-agent-status', []);
+      if (exitCode !== 0) {
+        throw Error(stderr);
+      }
 
-    async install(path: PathLike): Promise<InstallResult> {
-        if (this.usesPoetry(path)) {
-            const installed = await this.isInstalled(path)
-            const cmd = installed? ['update'] : ['add', '--dev'];
-            console.log(`install, cmd ${cmd}`);
-            const pkg = '../../applandinc/appmap-python/dist/appmap-0.0.0-py3-none-any.whl';
-            const {stderr, exitCode} = await exec('poetry', cmd.concat([pkg]), { cwd: path as string, output: true });
-            if (exitCode !== 0) {
-                throw Error(stderr);
-            }
-            return 'installed';
-        }
-        throw new Error("Method not implemented.");
+      return JSON.parse(stdout);
     }
-
-    async init(path: PathLike): Promise<InitResponse> {
-        const { stdout, stderr, exitCode } = await this.projectExec(path, 'appmap-agent-init', [], {
-          cwd: path as string,
-        });
-        console.log(`init ${path}`);
-    
-        if (exitCode !== 0) {
-            console.log(`init failed, ${stderr}`);
-            throw new Error(stderr);
-        }
-    
-        return this.writeConfig(path, stdout);
-    }
-
-    files(path: PathLike): Promise<FilesResponse> {
-        throw new Error("Method not implemented.");
-    }
-    async status(path: PathLike): Promise<StatusResponse> {
-        if (this.usesPoetry(path)) {
-            const { stdout, stderr, exitCode } = await this.projectExec(path, 'appmap-agent-status', []);
-            if (exitCode !== 0) {
-                throw Error(stderr);
-            }
-
-            return JSON.parse(stdout);
-        }
-        throw new Error("Method not implemented.");
-    }
-    test(path: PathLike): Promise<void> {
-        throw new Error("Method not implemented.");
-    }
-
+    throw new Error('Method not implemented.');
+  }
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  test(path: PathLike): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
 }
