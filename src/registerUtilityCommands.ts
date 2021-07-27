8924d6be36501b163cb70de73fe7c8e5733eae27
Sasha H <sashahdev23@gmail.com>
2021-07-27 20:39:31 +0300

    fix: reset QUICKSTART_DOCS_SEEN flag

--- src/registerUtilityCommands.ts
+++ src/registerUtilityCommands.ts
@@ -2,12 +2,12 @@ import { ScenarioProvider } from './scenarioViewer';
 import * as vscode from 'vscode';
 import { LinkTreeDataProvider } from './tree/linkTreeDataProvider';
 import RemoteRecording from './remoteRecording';
-import { QUICKSTART_SEEN } from './util';
+import { QUICKSTART_DOCS_SEEN } from './util';
 
 export function registerUtilityCommands(context: vscode.ExtensionContext): void {
   context.subscriptions.push(
     vscode.commands.registerCommand('appmap.resetUsageState', async () => {
-      context.globalState.update(QUICKSTART_SEEN, null);
+      context.globalState.update(QUICKSTART_DOCS_SEEN, null);
       ScenarioProvider.resetState(context);
       LinkTreeDataProvider.resetState(context);
       RemoteRecording.resetState(context);
