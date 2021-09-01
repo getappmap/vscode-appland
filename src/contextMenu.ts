1ade72ad5fb3d81c279bac3cfb3631cdb726bd92
Sasha H <sashahdev23@gmail.com>
2021-09-01 23:33:11 +0300

    feat: rename AppMap from context menu

--- src/contextMenu.ts
+++ src/contextMenu.ts
@@ -1,8 +1,10 @@
 import * as vscode from 'vscode';
+import { promises as fs } from 'fs';
 
 interface TreeItemContext {
   descriptor: {
     resourceUri: vscode.Uri;
+    metadata: Record<string, unknown>;
   };
 }
 
@@ -23,6 +25,31 @@ export default class ContextMenu {
           vscode.commands.executeCommand('vscode.openWith', item.descriptor.resourceUri, 'default');
         }
       )
+    );
+    context.subscriptions.push(
+      vscode.commands.registerCommand('appmap.context.rename', async (item: TreeItemContext) => {
+        const newName = await vscode.window.showInputBox({
+          placeHolder: 'Enter AppMap name',
+          value: item.descriptor.metadata.name as string,
+        });
+
+        if (!newName) {
+          return;
+        }
+
+        try {
+          const file = await fs.readFile(item.descriptor.resourceUri.fsPath);
+          const content = JSON.parse(file.toString());
+
+          content.metadata.name = newName;
+
+          fs.writeFile(item.descriptor.resourceUri.fsPath, JSON.stringify(content));
+        } catch (e) {
+          vscode.window.showErrorMessage(
+            `Error while changing AppMap name: ${e.name}: ${e.message}`
+          );
+        }
+      })
     );
   }
 }
