3fb2d4ef0389c420814ed5fcf4fa5e1fcae33268
Dustin Byrne <dustinsbyrne@gmail.com>
2021-09-20 23:09:07 -0400

    fix: Disable uploading AppMaps from within the app

--- web/src/appmapView.js
+++ web/src/appmapView.js
@@ -15,7 +15,7 @@ export default function mountApp() {
       return h(VVsCodeExtension, {
         ref: 'ui',
         props: {
-          appMapUploadable: true,
+          appMapUploadable: false,
         },
       });
     },
