5e99a481f2572fb8c968d86f9f47758f0dc6f149
Dustin Byrne <dustinsbyrne@gmail.com>
2022-02-08 17:38:14 -0500

    fix: Upgrade @appland/components to v1.23.0

--- types/@appland/index.d.ts
+++
@@ -1,9 +0,0 @@
-declare module '@appland/models' {
-  class AppMap {}
-  class AppMapBuilder {
-    normalize(): AppMapBuilder;
-    build(): AppMap;
-  }
-
-  export function buildAppMap(data: string | Record<string, unknown>): AppMapBuilder;
-}
