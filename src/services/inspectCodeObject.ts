16b45ebc5b27b48bcfa61a892569270f1560cd87
Kevin Gilpin <kgilpin@gmail.com>
2022-05-18 16:02:26 -0400

    feat: Inspect code object via CLI shell-out

---
+++ src/services/inspectCodeObject.ts
@@ -0,0 +1,51 @@
+import { spawn } from 'child_process';
+import * as vscode from 'vscode';
+import commandArgs from './commandArgs';
+
+export type InspectResult = {
+  ancestors: string[];
+  callers: string[];
+  classTrigrams: string[];
+  functionTrigrams: string[];
+  packageTrigrams: string[];
+  httpServerRequests: string[];
+  returnValues: string[];
+  sqlQueries: string[];
+  sqlTables: string[];
+};
+
+class Inspector {
+  constructor(public folder: vscode.WorkspaceFolder, public fqid: string) {}
+
+  async inspect(): Promise<InspectResult> {
+    const args = ['appmap', 'inspect', this.fqid];
+
+    const command = await commandArgs(this.folder, args, {});
+
+    const response: string[] = [];
+    const process = spawn(command.mainCommand, command.args, command.options);
+    return new Promise<InspectResult>((resolve, reject) => {
+      process.once('exit', (code) => {
+        if (code === 0) return resolve(JSON.parse(response.join()) as InspectResult);
+        reject(code);
+      });
+
+      if (!process.stdout) {
+        console.log(`No stdout for ${command.mainCommand}`);
+        return reject(-1);
+      }
+      process.stdout.setEncoding('utf8');
+      process.stdout.on('data', (data) => {
+        response.push(data);
+      });
+    });
+  }
+}
+
+export default async function inspectCodeObject(
+  folder: vscode.WorkspaceFolder,
+  fqid: string
+): Promise<InspectResult> {
+  const inspector = new Inspector(folder, fqid);
+  return await inspector.inspect();
+}
