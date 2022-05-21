496b2cf974634dde882f3714a06e61d45c73d707
Kevin Gilpin <kgilpin@gmail.com>
2022-05-21 09:13:46 -0400

    fix: Search parent folders for npm/yarn lock files

--- src/configuration/packageManager.ts
+++ src/configuration/packageManager.ts
@@ -1,9 +1,19 @@
+import { parse, resolve } from 'path';
 import { Uri } from 'vscode';
-import { resolveFilePath } from '../util';
+import { fileExists } from '../util';
 
 export async function packageManagerCommand(folder: Uri): Promise<string[]> {
-  const npmLockFile = await resolveFilePath(folder.fsPath, 'package-lock.json');
-  const yarnLockFile = await resolveFilePath(folder.fsPath, 'yarn.lock');
+  const dirWithFile = async (dirName: string, fileName: string): Promise<string | undefined> => {
+    const filePath = resolve(dirName, fileName);
+    if (await fileExists(filePath)) return filePath;
+
+    if (dirName === parse(dirName).root) return;
+
+    return dirWithFile(resolve(dirName, '..'), fileName);
+  };
+
+  const npmLockFile = await dirWithFile(folder.fsPath, 'package-lock.json');
+  const yarnLockFile = await dirWithFile(folder.fsPath, 'yarn.lock');
 
   const args: string[] = [];
   if (yarnLockFile) {
@@ -13,6 +23,8 @@ export async function packageManagerCommand(folder: Uri): Promise<string[]> {
     args.unshift('--');
     args.unshift('exec');
     args.unshift('npm');
+  } else {
+    console.warn(`Neither package-lock.json nor yarn.lock found in ${folder.fsPath}`);
   }
   return args;
 }
