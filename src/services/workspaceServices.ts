f519c911891edc1b2362e651b2c8697456391e9a
Kevin Gilpin <kgilpin@gmail.com>
2022-06-17 15:55:01 -0400

    refactor: Remove unused type

--- src/services/workspaceServices.ts
+++ src/services/workspaceServices.ts
@@ -1,8 +1,6 @@
 import * as vscode from 'vscode';
 import { WorkspaceService, WorkspaceServiceInstance } from './workspaceService';
 
-type Constructor<T> = new (...args: any[]) => T;
-
 export default class WorkspaceServices {
   workspaceServices: WorkspaceService[] = [];
   workspaceServiceInstances: Record<string, WorkspaceServiceInstance[]> = {};
