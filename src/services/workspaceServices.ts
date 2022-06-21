eab066cf5fb1764b1899e2aa6aeb5ecc8ad969d7
Kevin Gilpin <kgilpin@gmail.com>
2022-06-21 13:06:28 -0400

    feat: WorkspaceServices is a singleton
    Registered instances are available via functions.

--- src/services/workspaceServices.ts
+++ src/services/workspaceServices.ts
@@ -1,9 +1,14 @@
+import assert from 'assert';
 import * as vscode from 'vscode';
 import { WorkspaceService, WorkspaceServiceInstance } from './workspaceService';
 
-export default class WorkspaceServices {
-  workspaceServices: WorkspaceService[] = [];
-  workspaceServiceInstances: Record<string, WorkspaceServiceInstance[]> = {};
+class WorkspaceServices {
+  workspaceServices: WorkspaceService<WorkspaceServiceInstance>[] = [];
+  workspaceServiceInstances: Map<vscode.WorkspaceFolder, WorkspaceServiceInstance[]> = new Map();
+  instanceServices: Map<
+    WorkspaceServiceInstance,
+    WorkspaceService<WorkspaceServiceInstance>
+  > = new Map();
 
   constructor(public context: vscode.ExtensionContext) {
     const folderAdded = (folder: vscode.WorkspaceFolder) => {
@@ -12,12 +17,12 @@ export default class WorkspaceServices {
           `Creating workspace service ${service.constructor.name} for ${folder.uri.toString()}`
         );
         const instance = await service.create(folder);
-        this.enrollServiceInstance(folder, instance);
+        this.enrollServiceInstance(folder, instance, service);
       });
     };
 
     const folderRemoved = (folder: vscode.WorkspaceFolder) => {
-      const serviceInstances = this.workspaceServiceInstances[folder.uri.toString()];
+      const serviceInstances = this.workspaceServiceInstances.get(folder);
       if (!serviceInstances) return;
 
       serviceInstances.forEach((service) => {
@@ -39,7 +44,9 @@ export default class WorkspaceServices {
     (vscode.workspace.workspaceFolders || []).forEach(folderAdded);
   }
 
-  async enroll(service: WorkspaceService): Promise<WorkspaceServiceInstance[]> {
+  async enroll(
+    service: WorkspaceService<WorkspaceServiceInstance>
+  ): Promise<WorkspaceServiceInstance[]> {
     this.workspaceServices.push(service);
 
     return Promise.all(
@@ -48,19 +55,62 @@ export default class WorkspaceServices {
           `Creating workspace service ${service.constructor.name} for ${folder.uri.toString()}`
         );
         const instance = await service.create(folder);
-        this.enrollServiceInstance(folder, instance);
+        this.enrollServiceInstance(folder, instance, service);
         return instance;
       })
     );
   }
 
+  /**
+   * Gets the single service instance (or `undefined`) for a given folder.
+   */
+  getServiceInstance(
+    service: WorkspaceService<WorkspaceServiceInstance>,
+    folder: vscode.WorkspaceFolder
+  ): WorkspaceServiceInstance | undefined {
+    const serviceInstances = this.getServiceInstances(service, folder);
+    return serviceInstances.length > 0 ? serviceInstances[0] : undefined;
+  }
+
+  /**
+   * Gets all instances of a given service, with an optional folder name.
+   */
+  getServiceInstances(
+    service: WorkspaceService<WorkspaceServiceInstance>,
+    folder?: vscode.WorkspaceFolder
+  ): WorkspaceServiceInstance[] {
+    const serviceInstances = folder
+      ? this.workspaceServiceInstances.get(folder) || []
+      : Array(...this.workspaceServiceInstances.values()).flat();
+    return serviceInstances.filter((instance) => this.instanceServices.get(instance) === service);
+  }
+
   private enrollServiceInstance(
     folder: vscode.WorkspaceFolder,
-    instance: WorkspaceServiceInstance
+    instance: WorkspaceServiceInstance,
+    service: WorkspaceService<WorkspaceServiceInstance>
   ) {
     this.context.subscriptions.push(instance);
-    if (!this.workspaceServiceInstances[folder.uri.toString()])
-      this.workspaceServiceInstances[folder.uri.toString()] = [];
-    this.workspaceServiceInstances[folder.uri.toString()].push(instance);
+    let instances = this.workspaceServiceInstances.get(folder);
+    if (!instances) {
+      instances = [];
+      this.workspaceServiceInstances.set(folder, instances);
+    }
+    instances.push(instance);
+    this.instanceServices.set(instance, service);
   }
+}
+
+let WORKSPACE_SERVICES: WorkspaceServices | undefined;
+
+export function initializeWorkspaceServices(context: vscode.ExtensionContext): WorkspaceServices {
+  if (!WORKSPACE_SERVICES) {
+    WORKSPACE_SERVICES = new WorkspaceServices(context);
+  }
+  return WORKSPACE_SERVICES;
+}
+
+export function workspaceServices(): WorkspaceServices {
+  assert(WORKSPACE_SERVICES, 'WorkspaceServices is not initialized');
+  return WORKSPACE_SERVICES;
 }
