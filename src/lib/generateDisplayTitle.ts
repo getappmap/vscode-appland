a643b78041e8b6d570bd2d995b01e594b8d53059
MaxDillon <maxdillon13124@gmail.com>
2022-06-22 14:51:42 -0700

    fix: Create getTreeName function to format tree name

---
+++ src/lib/generateDisplayTitle.ts
@@ -0,0 +1,20 @@
+import { ResolvedFinding } from '../services/resolvedFinding';
+import * as vscode from 'vscode';
+
+// Gets's name displayed in Findings bar
+export default (finding: ResolvedFinding): string => {
+  const absPath = finding.problemLocation?.uri.path;
+  const relPath = absPath ? vscode.workspace.asRelativePath(absPath) : undefined;
+
+  const rule = finding.finding.ruleTitle;
+  const context = finding.finding.groupMessage || finding.finding.message;
+  const lineno = finding.problemLocation?.range.start.line;
+
+  // If rule and context are the same, only display one
+  const ruleAndContext = rule !== context ? `${rule}: ${context}` : rule;
+
+  // Only display problemLocation if it exists
+  const fullPathString = finding.problemLocation ? `, ${relPath}:${lineno}` : '';
+
+  return `${ruleAndContext}${fullPathString}`;
+};
