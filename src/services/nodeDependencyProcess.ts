73d0254b541ed011ba4587a512c039786d357a2c
Dustin Byrne <dustinsbyrne@gmail.com>
2022-06-24 14:39:08 -0400

    refactor: Add a generic process spawner for Electron node and node.js dependencies

---
+++ src/services/nodeDependencyProcess.ts
@@ -0,0 +1,163 @@
+import * as childProcess from 'child_process';
+import * as vscode from 'vscode';
+import * as path from 'path';
+
+export enum Dependency {
+  Appmap = 'appmap',
+  Scanner = 'scanner',
+}
+
+export type WithLogCache = {
+  log: ProcessLog;
+};
+
+export type ChildProcess = childProcess.ChildProcessWithoutNullStreams & WithLogCache;
+
+export type SpawnOptions = {
+  // If specified, resolves the path to a bin script installed via yarn and adds it to the command args
+  // to be run via `node`
+  bin?: DependencyOptions;
+
+  // Command line args given to `node` or the `bin` script specified
+  args?: string[];
+
+  // If specified, write log messages to the given output channel
+  log?: vscode.OutputChannel;
+
+  // Indicates whether or not log messages should be retained in a buffer
+  cacheLog?: boolean;
+} & Exclude<childProcess.SpawnOptionsWithoutStdio, 'argv0'>;
+
+export type DependencyOptions = {
+  // Name of the dependency to be resolved to a bin script path
+  dependency: Dependency;
+
+  // This is neccesary for `yarn` to locate the bin script
+  globalStoragePath: string;
+};
+
+export enum OutputStream {
+  Stdout,
+  Stderr,
+}
+
+export type ProcessLogItem = { stream: OutputStream; data: string };
+export class ProcessLog extends Array<ProcessLogItem> {
+  protected constructor(
+    protected process: childProcess.ChildProcessWithoutNullStreams,
+    protected outputChannel?: vscode.OutputChannel,
+    protected shouldCache?: boolean
+  ) {
+    super();
+  }
+
+  static appendLogger(
+    process: childProcess.ChildProcessWithoutNullStreams,
+    outputChannel?: vscode.OutputChannel,
+    cache?: boolean
+  ): ChildProcess {
+    if ((process as ChildProcess).log) {
+      throw new Error(`process ${process.pid} already has a logger appended`);
+    }
+
+    const log = new ProcessLog(process, outputChannel, cache);
+
+    process.stdout.setEncoding('utf8');
+    process.stderr.setEncoding('utf8');
+    process.stdout.on('data', (data) => log.appendLines(data, OutputStream.Stdout, true));
+    process.stderr.on('data', (data) => log.appendLines(data, OutputStream.Stderr, true));
+    process.once('error', (err) =>
+      log.append(`an error occurred: ${String(err)}`, OutputStream.Stderr)
+    );
+    process.once('exit', (code, signal) => {
+      const msg: string[] = ['exited'];
+      if (code || !signal) {
+        msg.push(`with code ${code || 0}`);
+      } else {
+        msg.push(`due to signal ${signal}`);
+      }
+      log.append(msg.join(' '), code === 0 ? OutputStream.Stdout : OutputStream.Stderr);
+    });
+
+    const modifiedProcess = process as ChildProcess;
+    modifiedProcess.log = log;
+
+    return modifiedProcess;
+  }
+
+  protected appendLines(data: string, stream = OutputStream.Stdout, cache = false): void {
+    data
+      .trim()
+      .split('\n')
+      .forEach((l) => this.append(l.trim(), stream, cache));
+  }
+
+  append(data: string, stream = OutputStream.Stdout, cache = false): void {
+    if (this.shouldCache && cache) {
+      this.push({ stream, data });
+    }
+    if (this.outputChannel) {
+      this.outputChannel.appendLine(`${this.process?.pid} [${OutputStream[stream]}] ${data}`);
+    }
+  }
+
+  toString(): string {
+    return this.map((log) => `${log.stream}: ${log.data}`).join('\n');
+  }
+}
+
+async function getBinPath(options: DependencyOptions): Promise<string> {
+  const yarnPath = path.join(options.globalStoragePath, 'yarn.js');
+  const process = await spawn({
+    args: [yarnPath, 'bin', options.dependency],
+    cwd: options.globalStoragePath,
+    cacheLog: true,
+  });
+  return new Promise((resolve, reject) => {
+    process.once('error', (err) => reject(err));
+    process.once('exit', async (code) => {
+      // There seem to be intermittent times where `exit` is handled before `data`,
+      // meaning the output of this command will not yet have been recorded by the
+      // time this event was fired. Push this handler to the back of the async queue.
+      await new Promise((resolve) => setTimeout(resolve, 0));
+
+      if (code && code !== 0) {
+        const message = [
+          `failed to fetch bin path for ${options.dependency}`,
+          process.log.toString(),
+        ].join('\n');
+        throw new Error(message);
+      }
+
+      const result = process.log
+        .filter((l) => l.stream === OutputStream.Stdout)
+        .map((l) => l.data)
+        .join('\n');
+      resolve(result);
+    });
+  });
+}
+
+export async function spawn(options: SpawnOptions): Promise<ChildProcess> {
+  const nodePath: string = process.argv0;
+  const additionalEnv: NodeJS.ProcessEnv = process.env;
+  const additionalArgs: string[] = [];
+  const isElectronApp = !vscode.env.remoteName;
+
+  if (isElectronApp) {
+    additionalEnv['ELECTRON_RUN_AS_NODE'] = 'true';
+    additionalArgs.push('--ms-enable-electron-run-as-node');
+  }
+
+  if (options.bin) {
+    const binPath = await getBinPath(options.bin);
+    additionalArgs.push(binPath);
+  }
+
+  const args = [...additionalArgs, ...(options.args || [])];
+  const env = { ...process.env, ...additionalEnv, ...(options.env || {}) };
+  const newProcess = childProcess.spawn(nodePath, args, { ...options, env });
+  const loggedProcess = ProcessLog.appendLogger(newProcess, options.log, options.cacheLog);
+
+  return loggedProcess;
+}
--- src/services/nodeProcessService.ts
+++ src/services/nodeProcessService.ts
@@ -1,6 +1,5 @@
 import * as vscode from 'vscode';
 import * as path from 'path';
-import NodeRunner, { ProcessOutputType } from './nodeRunner';
 import { DEBUG_EXCEPTION, Telemetry } from '../telemetry';
 import { promises as fs } from 'fs';
 import { WorkspaceService } from './workspaceService';
@@ -8,17 +7,17 @@ import NodeProcessServiceInstance from './nodeProcessServiceInstance';
 import { ProcessWatcher } from './processWatcher';
 import ExtensionSettings from '../configuration/extensionSettings';
 import ErrorCode from '../telemetry/definitions/errorCodes';
+import { Dependency, spawn } from './nodeDependencyProcess';
 
 const YARN_JS = 'yarn.js';
 
 export class NodeProcessService implements WorkspaceService<NodeProcessServiceInstance> {
-  protected node: NodeRunner;
   protected externDir: string;
   protected globalStorageDir: string;
   protected COPY_FILES: string[] = ['package.json', 'yarn.lock', YARN_JS];
+  protected static outputChannel = vscode.window.createOutputChannel('AppMap: Services');
 
   constructor(context: vscode.ExtensionContext) {
-    this.node = new NodeRunner(context.globalStorageUri.fsPath);
     this.globalStorageDir = context.globalStorageUri.fsPath;
     this.externDir = path.join(context.extensionPath, 'extern');
   }
@@ -28,11 +27,14 @@ export class NodeProcessService implements WorkspaceService<NodeProcessServiceIn
 
     if (ExtensionSettings.indexEnabled()) {
       try {
-        const appmapBinPath = await this.binPath('appmap');
         services.push(
           new ProcessWatcher({
-            args: [this.nodeArgs, appmapBinPath, ExtensionSettings.indexCommand()].flat(),
-            env: this.env,
+            bin: {
+              dependency: Dependency.Appmap,
+              globalStoragePath: this.globalStorageDir,
+            },
+            log: NodeProcessService.outputChannel,
+            args: [ExtensionSettings.indexCommand()].flat(),
             cwd: folder.uri.fsPath,
           })
         );
@@ -46,11 +48,14 @@ export class NodeProcessService implements WorkspaceService<NodeProcessServiceIn
 
     if (ExtensionSettings.findingsEnabled()) {
       try {
-        const scannerBinPath = await this.binPath('scanner');
         services.push(
           new ProcessWatcher({
-            args: [this.nodeArgs, scannerBinPath, ExtensionSettings.scanCommand()].flat(),
-            env: this.env,
+            bin: {
+              dependency: Dependency.Scanner,
+              globalStoragePath: this.globalStorageDir,
+            },
+            log: NodeProcessService.outputChannel,
+            args: [ExtensionSettings.scanCommand()].flat(),
             cwd: folder.uri.fsPath,
           })
         );
@@ -65,43 +70,8 @@ export class NodeProcessService implements WorkspaceService<NodeProcessServiceIn
     return new NodeProcessServiceInstance(folder, services);
   }
 
-  protected get isElectronApp(): boolean {
-    return !vscode.env.remoteName;
-  }
-
-  protected get nodeArgs(): string[] {
-    return [process.argv[0], this.isElectronApp && '--ms-enable-electron-run-as-node'].filter(
-      Boolean
-    ) as string[];
-  }
-
-  protected get env(): NodeJS.ProcessEnv {
-    const additionalEnv = { ...process.env };
-    if (this.isElectronApp) {
-      additionalEnv['ELECTRON_RUN_AS_NODE'] = 'true';
-    }
-    return additionalEnv;
-  }
-
   protected get yarnPath(): string {
     return path.join(this.globalStorageDir, YARN_JS);
-  }
-
-  async binPath(dependency: string): Promise<string> {
-    const output = await this.node.exec(this.yarnPath, 'bin', dependency);
-    if (output.exitCode && output.exitCode !== 0) {
-      throw new Error(
-        [
-          `failed to fetch bin path for ${dependency}`,
-          output.log.map((l) => `[${l.type}] ${l.data}`),
-        ].join('\n')
-      );
-    }
-    return output.log
-      .filter((l) => l.type === ProcessOutputType.Stdout)
-      .map((l) => l.data)
-      .join('\n')
-      .trim();
   }
 
   async install(): Promise<void> {
@@ -122,16 +92,31 @@ export class NodeProcessService implements WorkspaceService<NodeProcessServiceIn
         ['nodeLinker: node-modules', 'npmRegistryServer: "https://registry.npmjs.org"'].join('\n')
       );
 
-      const processOutput = await this.node.exec(this.yarnPath);
-      if (processOutput.signal || (processOutput.exitCode && processOutput.exitCode !== 0)) {
-        const message = [
-          'Failed to install dependencies',
-          `Exit code: ${processOutput.exitCode}`,
-          `Signal: ${processOutput.signal}`,
-          ...processOutput.log.map((log) => `${log.type}: ${log.data}`),
-        ].join('\n');
-        throw new Error(message);
-      }
+      const installProcess = await spawn({
+        args: [this.yarnPath, 'install'],
+        cwd: this.globalStorageDir,
+        log: NodeProcessService.outputChannel,
+      });
+
+      installProcess.log.append('Installing dependencies...');
+
+      return new Promise((resolve, reject) => {
+        installProcess.once('error', (err) => reject(err));
+        installProcess.once('exit', (code, signal) => {
+          if (code && code !== 0) {
+            const message = [
+              'Failed to install dependencies',
+              `Exit code: ${code}`,
+              `Signal: ${signal}`,
+              installProcess.log.toString(),
+            ].join('\n');
+            reject(new Error(message));
+          }
+
+          installProcess.log.append('AppMap services are up to date.');
+          resolve();
+        });
+      });
     } catch (e) {
       const err =
         e instanceof Error ? e : new Error(`failed to install node dependencies: ${String(e)}`);
