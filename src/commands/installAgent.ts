b395a6d659990d5ced68723357669f4b7702c866
Dustin Byrne <dustinsbyrne@gmail.com>
2022-10-05 11:28:16 -0400

    fixup! escape shell paths

--- src/commands/installAgent.ts
+++ src/commands/installAgent.ts
@@ -1,11 +1,14 @@
 import * as vscode from 'vscode';
 import os from 'os';
+import { join } from 'path';
 import { CLICK_INSTALL_BUTTON, INSTALL_BUTTON_ERROR, Telemetry } from '../telemetry';
 import { NodeProcessService } from '../services/nodeProcessService';
 
 export const InstallAgent = 'appmap.installAgent';
 const ELECTRON_COMMAND_PLATFORMS = ['linux', 'darwin'];
 let pendingTask: Thenable<unknown> | undefined;
+
+const escape = (str) => str.replace(/([^A-Za-z0-9_\-.,:/@\n])/g, '\\$1');
 
 function formatProjectPath(path: string): string {
   if (path.includes(' ')) {
@@ -15,9 +18,9 @@ function formatProjectPath(path: string): string {
 }
 
 function electronCommand(globalStorageDir: string, installLocation: string): string {
-  const nodePath = process.argv0;
-  const cliPath = `${globalStorageDir}/node_modules/@appland/appmap/built/cli.js`;
-  const flags = ['--ms-enable-electron-run-as-node', '-d', installLocation];
+  const nodePath = escape(process.argv0);
+  const cliPath = `${escape(globalStorageDir)}/node_modules/@appland/appmap/built/cli.js`;
+  const flags = ['--ms-enable-electron-run-as-node', '-d', escape(installLocation)];
   return `ELECTRON_RUN_AS_NODE=true ${nodePath} ${cliPath} install ${flags.join(' ')}`;
 }
 
@@ -70,7 +73,8 @@ export default async function installAgent(
           env['ELECTRON_RUN_AS_NODE'] = 'true';
           command = electronCommand(context.globalStorageUri.fsPath, installLocation);
         } else if (os.platform() === 'win32' && hasCLIBin) {
-          command = `${context.globalStorageUri.fsPath}\\appmap-win-x64.exe install -d ${installLocation}`;
+          const binPath = join(escape(context.globalStorageUri.fsPath), 'appmap-win-x64.exe');
+          command = `${binPath} install -d ${installLocation}`;
         }
       }
 
