488d56030e7a111120599d62c7944df3e29f2317
Kevin Gilpin <kgilpin@gmail.com>
2022-10-22 23:39:58 -0400

    feat: Compare two sequence diagrams

---
+++ src/commands/compareSequenceDiagram.ts
@@ -0,0 +1,79 @@
+import * as vscode from 'vscode';
+import * as childProcess from 'child_process';
+import { readFile, writeFile } from 'fs/promises';
+
+import { AppMap, buildAppMap } from '@appland/models';
+import buildDiagram from '@appland/sequence-diagram/dist/buildDiagram';
+import diff from '@appland/sequence-diagram/dist/diff';
+import buildDiffDiagram from '@appland/sequence-diagram/dist/buildDiffDiagram';
+import Specification from '@appland/sequence-diagram/dist/specification';
+import { Diagram } from '@appland/sequence-diagram/dist/types';
+import { format as formatPlantUML } from '@appland/sequence-diagram/dist/formatter/plantUML';
+
+import { verifyCommandOutput } from '../services/nodeDependencyProcess';
+import AppMapCollection from '../services/appmapCollection';
+import { plantUMLJarPath, promptForAppMap, promptForSpecification } from '../lib/sequenceDiagram';
+import { tmpName } from 'tmp';
+import { promisify } from 'util';
+
+export default async function compareSequenceDiagrams(
+  context: vscode.ExtensionContext,
+  appmaps: AppMapCollection
+): Promise<void> {
+  const command = vscode.commands.registerCommand(
+    'appmap.compareSequenceDiagrams',
+    async (baseAppMapUri: vscode.Uri, headAppMapUri: vscode.Uri) => {
+      const umlJar = plantUMLJarPath();
+      if (!umlJar) return;
+
+      const diffDiagrams = async (): Promise<{ base: Diagram; head: Diagram } | undefined> => {
+        const uris = [baseAppMapUri, headAppMapUri];
+        let specification: Specification | undefined;
+        const diagrams: Diagram[] = [];
+        for (let index = 0; index < uris.length; index++) {
+          let appmapUri = uris[index];
+          if (!appmapUri) {
+            const appmap = await promptForAppMap(appmaps.appMaps());
+            if (!appmap) return;
+
+            appmapUri = appmap.descriptor.resourceUri;
+          }
+          const data = await readFile(appmapUri.fsPath, 'utf-8');
+          const appmap: AppMap = buildAppMap()
+            .source(data)
+            .build();
+
+          if (!specification) specification = await promptForSpecification(appmap);
+
+          const diagram = buildDiagram(appmapUri.fsPath, appmap, specification);
+          diagrams.push(diagram);
+        }
+        return { base: diagrams[0], head: diagrams[1] };
+      };
+
+      vscode.window.withProgress(
+        { location: vscode.ProgressLocation.Notification, title: 'Generating sequence diagram' },
+        async () => {
+          const diagrams = await diffDiagrams();
+          if (!diagrams) return;
+
+          const { base, head } = diagrams;
+          const diffRaw = diff(base, head, { verbose: false });
+          const diffDiagram = buildDiffDiagram(diffRaw);
+
+          const uml = formatPlantUML(diffDiagram, 'base -> head');
+          const diagramFile = await promisify(tmpName)();
+          await writeFile(diagramFile, uml);
+
+          const cmd = childProcess.spawn('java', ['-jar', umlJar, '-tsvg', diagramFile]);
+          await verifyCommandOutput(cmd);
+
+          const svgFile = [diagramFile, 'svg'].join('.');
+          vscode.env.openExternal(vscode.Uri.file(svgFile));
+        }
+      );
+
+      context.subscriptions.push(command);
+    }
+  );
+}
---
+++ src/lib/sequenceDiagram.ts
@@ -0,0 +1,138 @@
+import { AppMap, CodeObject } from '@appland/models';
+import Priority from '@appland/sequence-diagram/dist/priority';
+import Specification from '@appland/sequence-diagram/dist/specification';
+import assert from 'assert';
+import * as vscode from 'vscode';
+
+import ExtensionSettings from '../configuration/extensionSettings';
+import AppMapLoader from '../services/appmapLoader';
+
+// TODO: Augment or replace these with filters that the user has applied in the AppMap diagram.
+const IGNORE_PACKAGES: Record<string, string[]> = {
+  ruby: [
+    'actionpack',
+    'activesupport',
+    'activerecord',
+    'actionview',
+    'json',
+    'logger',
+    'openssl',
+    'ruby',
+    'sprockets',
+  ],
+};
+
+export function plantUMLJarPath(): string | undefined {
+  const result = ExtensionSettings.plantUMLJarPath;
+  if (!result) {
+    vscode.window.showErrorMessage(
+      `Setting "AppMap : Plant UML Jar Path" is required to generate sequence diagrams`
+    );
+    return;
+  }
+  return result;
+}
+
+type AppMapQuickPickItem = vscode.QuickPickItem & {
+  appmap: AppMapLoader;
+};
+
+export async function promptForAppMap(appmaps: AppMapLoader[]): Promise<AppMapLoader | undefined> {
+  const items = appmaps
+    .filter((appmap) => appmap.descriptor.metadata?.name)
+    .map(
+      (appmap) =>
+        ({
+          appmap,
+          label: appmap.descriptor.metadata?.name,
+        } as AppMapQuickPickItem)
+    );
+  const result = await vscode.window.showQuickPick<AppMapQuickPickItem>(items);
+  if (!result) return;
+
+  return result.appmap;
+}
+
+export async function promptForSpecification(appmap: AppMap): Promise<Specification> {
+  const language = appmap.metadata.language?.name || 'unknownLanguage';
+  const suggestedIgnorePackages = IGNORE_PACKAGES[language] || [];
+
+  const ignorePackagesText = await vscode.window.showInputBox({
+    title: 'Enter packages to exclude from the diagram',
+    value: ExtensionSettings.getSequenceDiagramSetting(
+      language,
+      'ignorePackages',
+      suggestedIgnorePackages.join(' ')
+    ),
+  });
+
+  ExtensionSettings.setSequenceDiagramSetting('ignorePackages', language, ignorePackagesText || '');
+
+  const ignorePackages = (ignorePackagesText
+    ? ignorePackagesText.split(/\s+/)
+    : suggestedIgnorePackages
+  ).map((pkg) => ['package', pkg].join(':'));
+
+  const expandPackagesText = await vscode.window.showInputBox({
+    title: 'Enter packages to expand into classes',
+    value: ExtensionSettings.getSequenceDiagramSetting(language, 'expandPackages', ''),
+  });
+
+  ExtensionSettings.setSequenceDiagramSetting('expandPackages', language, expandPackagesText || '');
+
+  const expandPackages = (expandPackagesText ? expandPackagesText.split(/\s+/) : []).map((pkg) =>
+    ['package', pkg].join(':')
+  );
+
+  // Include HTTP, all packages, and the database
+  const includedCodeObjectIds = new Set<string>();
+  includedCodeObjectIds.add('http:HTTP server requests');
+  includedCodeObjectIds.add('database:Database');
+
+  const packageNames: string[] = [];
+  const requiredCodeObjectIds = new Set<string>();
+  const classesForPackage = new Map<string, string[]>();
+  appmap.classMap.visit((co: CodeObject) => {
+    if (co.type === 'package' && !ignorePackages.includes(co.fqid)) {
+      packageNames.push(co.fqid);
+    }
+    if (co.type === 'class') {
+      let pkg = co.packageObject;
+      while (pkg) {
+        let classes: string[] | undefined = classesForPackage.get(pkg.fqid);
+        if (!classes) {
+          classes = [];
+          classesForPackage.set(pkg.fqid, classes);
+        }
+        classes.push(co.fqid);
+        pkg = pkg.parent;
+      }
+    }
+  });
+
+  packageNames
+    .filter((pkg) => expandPackages.includes(pkg))
+    .forEach((pkg) => {
+      const classes = classesForPackage.get(pkg);
+      assert(classes);
+      classes.forEach((cls) => requiredCodeObjectIds.add(cls));
+    });
+  packageNames
+    .filter((pkg) => !expandPackages.includes(pkg))
+    .forEach((pkg) => requiredCodeObjectIds.add(pkg));
+
+  const priority = new Priority();
+  priority.enrollPattern('http:%r{.*}');
+  priority.expandPattern('http:%r{.*}', ['http:HTTP server requests']);
+
+  priority.enrollPattern('package:%r{.*}');
+  priority.expandPattern('package:%r{.*}', packageNames);
+
+  priority.enrollPattern('database:%r{.*}');
+  priority.expandPattern('database:%r{.*}', ['database:Database']);
+
+  for (const coid of requiredCodeObjectIds) {
+    includedCodeObjectIds.add(coid);
+  }
+  return new Specification(priority, includedCodeObjectIds, requiredCodeObjectIds);
+}
