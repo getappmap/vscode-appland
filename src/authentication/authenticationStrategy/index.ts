e24c793485f98cb31bc357b0400e3fb8fc9dd17d
Dustin Byrne <dustinsbyrne@gmail.com>
2022-11-09 11:55:24 -0500

    Drop the optional Promise from `prepareSignIn`

--- src/authentication/authenticationStrategy/index.ts
+++ src/authentication/authenticationStrategy/index.ts
@@ -3,5 +3,5 @@ import { Disposable } from 'vscode';
 export default interface AuthenticationStrategy extends Disposable {
   authnPath: string;
   redirectUrl(params: ReadonlyArray<[string, string]>): string | Promise<string>;
-  prepareSignIn(): void | Promise<void>;
+  prepareSignIn(): void;
 }
