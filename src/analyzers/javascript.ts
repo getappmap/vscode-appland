dba3f584486aa1ddff6432c007e4001fa39668bb
Rafał Rzepecki <divided.mind@gmail.com>
2023-04-18 16:00:17 +0200

    feat: Recognize jest support in the project picker

--- src/analyzers/javascript.ts
+++ src/analyzers/javascript.ts
@@ -1,7 +1,8 @@
 import { Uri, workspace, WorkspaceFolder } from 'vscode';
-import { Features, ProjectAnalysis, overallScore } from '.';
+import { Features, ProjectAnalysis, overallScore, Feature } from '.';
 import utfDecoder from '../utfDecoder';
 import semverIntersects from 'semver/ranges/intersects';
+import assert from 'assert';
 
 const fs = workspace.fs;
 
@@ -17,11 +18,20 @@ export default async function analyze(folder: WorkspaceFolder): Promise<ProjectA
     },
   };
 
-  let pkg: any; // eslint-disable-line @typescript-eslint/no-explicit-any
   try {
-    const file = await fs.readFile(Uri.joinPath(folder.uri, 'package.json'));
-    const json = utfDecoder(file);
-    pkg = JSON.parse(json);
+    const { dependencies, devDependencies } = await readPkg(folder.uri);
+
+    if (dependencies?.express) {
+      features.web = {
+        title: 'express.js',
+        score: 'ok',
+        text: 'This project uses Express. AppMap will automatically recognize web requests, SQL queries, and key framework functions during recording.',
+      };
+    }
+
+    const testFeature =
+      detectTest(devDependencies, 'mocha', '>= 8') || detectTest(devDependencies, 'jest', '>= 25');
+    if (testFeature) features.test = testFeature;
   } catch (_) {
     features.lang = {
       title: 'JavaScript',
@@ -30,36 +40,45 @@ export default async function analyze(folder: WorkspaceFolder): Promise<ProjectA
     };
   }
 
-  if (pkg) {
-    if (pkg.dependencies?.express) {
-      features.web = {
-        title: 'express.js',
-        score: 'ok',
-        text: 'This project uses Express. AppMap will automatically recognize web requests, SQL queries, and key framework functions during recording.',
-      };
-    }
-
-    if (pkg.devDependencies?.mocha) {
-      const { mocha: mochaVersion } = pkg.devDependencies;
-      if (mochaVersion === 'latest' || semverIntersects('>=8', mochaVersion)) {
-        features.test = {
-          title: 'mocha',
-          score: 'ok',
-          text: `This project uses Mocha. Test execution can be automatically recorded.`,
-        };
-      } else {
-        features.test = {
-          title: 'mocha',
-          score: 'bad',
-          text: 'This project uses an unsupported version of Mocha. You need at least version 8 to automatically record test execution.',
-        };
-      }
-    }
-  }
-
   return {
     name: folder.name,
     features: features,
     score: overallScore(features),
   };
+}
+
+type Dependency = Record<string, string | undefined>;
+
+async function readPkg(uri: Uri): Promise<{
+  dependencies?: Dependency;
+  devDependencies?: Dependency;
+}> {
+  const file = await fs.readFile(Uri.joinPath(uri, 'package.json'));
+  const json = utfDecoder(file);
+  const pkg: unknown = JSON.parse(json);
+  assert(pkg && typeof pkg === 'object');
+  assert(!('dependencies' in pkg) || typeof pkg.dependencies === 'object');
+  assert(!('devDependencies' in pkg) || typeof pkg.devDependencies === 'object');
+  return pkg;
+}
+
+function detectTest(
+  dep: Dependency | undefined,
+  testPackage: string,
+  constraint: string
+): Feature | undefined {
+  const version = dep && dep[testPackage];
+  if (!version) return undefined;
+  if (version === 'latest' || semverIntersects(constraint, version))
+    return {
+      title: testPackage,
+      score: 'ok',
+      text: `This project uses ${testPackage}. Test execution can be automatically recorded.`,
+    };
+  else
+    return {
+      title: testPackage,
+      score: 'bad',
+      text: `This project uses an unsupported version of ${testPackage}. You need version ${constraint} to automatically record test execution.`,
+    };
 }
