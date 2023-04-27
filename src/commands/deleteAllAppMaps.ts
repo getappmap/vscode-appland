1d8b06451f23b6b3da8c1e71125ac695d4327043
Kevin Gilpin <kgilpin@gmail.com>
2023-04-27 14:40:36 -0400

    feat: Delete all AppMaps only from appmap_dir

--- src/commands/deleteAllAppMaps.ts
+++ src/commands/deleteAllAppMaps.ts
@@ -2,15 +2,34 @@ import * as vscode from 'vscode';
 import deleteAppMaps from '../lib/deleteAppMaps';
 import AnalysisManager from '../services/analysisManager';
 import ClassMapIndex from '../services/classMapIndex';
+import { AppmapConfigManager, AppmapConfigManagerInstance } from '../services/appmapConfigManager';
+import { workspaceServices } from '../services/workspaceServices';
+import assert from 'assert';
 
 export default function deleteAllAppMaps(
   context: vscode.ExtensionContext,
   classMapIndex?: ClassMapIndex
 ): void {
+  async function deleteWorkspaceAppMaps(folder: vscode.WorkspaceFolder) {
+    const appmapConfigManagerInstance = workspaceServices().getServiceInstanceFromClass(
+      AppmapConfigManager,
+      folder
+    ) as AppmapConfigManagerInstance | undefined;
+    assert(appmapConfigManagerInstance);
+
+    const appmapDir = (await appmapConfigManagerInstance.getAppmapConfig())?.appmapDir;
+    if (!appmapDir) {
+      console.warn(`No appmapDir for ${folder.uri}`);
+      return;
+    }
+
+    deleteAppMaps(appmapDir);
+  }
+
   context.subscriptions.push(
     vscode.commands.registerCommand('appmap.deleteAllAppMaps', async () => {
       await Promise.all(
-        (vscode.workspace.workspaceFolders || []).map((folder) => deleteAppMaps(folder.uri.fsPath))
+        (vscode.workspace.workspaceFolders || []).map((folder) => deleteWorkspaceAppMaps(folder))
       );
 
       const { findingsIndex } = AnalysisManager;
