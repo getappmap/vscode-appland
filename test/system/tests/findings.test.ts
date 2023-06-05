1348e3bc99a3c01b64cb1af72266d4fcf0e82017
Kevin Gilpin <kgilpin@gmail.com>
2023-06-05 12:12:11 -0400

    ci: Remove non-working xit test

--- test/system/tests/findings.test.ts
+++ test/system/tests/findings.test.ts
@@ -85,30 +85,4 @@ describe('Findings and scanning', function () {
     const expectedTitle = 'N plus 1 SQL query';
     await findingDetailsWebview.assertTitleRenders(expectedTitle);
   });
-
-  // TODO: Fix this test
-  xit('reuses the finding details webview', async function () {
-    const { driver, project } = this;
-    const findingsOverviewWebview = driver.appMap['findingsOverviewWebview'];
-    const findingDetailsWebview = driver.appMap['findingDetailsWebview'];
-
-    await driver.appMap.openActionPanel();
-    await driver.appMap.expandFindings();
-    await driver.appMap.openFindingsOverview();
-    let expectedFrames = 2;
-    await findingsOverviewWebview.initialize(expectedFrames);
-
-    await project.restoreFiles('**/*.appmap.json');
-    await driver.waitForFile(path.join(project.workspacePath, 'tmp', '**', 'mtime')); // Wait for the indexer
-    await driver.appMap.findingsTreeItem(1).waitFor();
-
-    await findingsOverviewWebview.openFirstFindingDetail();
-    expectedFrames = 3;
-    await findingDetailsWebview.initialize(expectedFrames);
-    await driver.appMap.openNthFinding(2);
-    await findingDetailsWebview.initialize(expectedFrames);
-
-    const numTabs = await driver.tabCount();
-    strictEqual(numTabs, expectedFrames);
-  });
 });
