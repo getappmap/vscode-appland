a1502d1e1a193851cef6b1676182709ec295a901
Adam Trotta <ahtrotta@gmail.com>
2023-06-15 14:17:49 -0400

    test: remove intermittently failing system tests
    Tests were added to appmap-js to compensate for these removed system
    tests.

--- test/system/tests/findings.test.ts
+++ test/system/tests/findings.test.ts
@@ -25,46 +25,4 @@ describe('Findings and scanning', function () {
     await driver.appMap.findingsTreeItem().waitFor();
     await driver.appMap.findingsTreeItem(1).waitFor();
   });
-
-  it('shows the findings overview page', async function () {
-    const { driver, project } = this;
-    const findingsOverviewWebview = driver.appMap['findingsOverviewWebview'];
-
-    await project.restoreFiles('**/*.appmap.json');
-    await driver.waitForFile(path.join(project.workspacePath, 'tmp', '**', 'mtime')); // Wait for the indexer
-
-    await driver.appMap.openActionPanel();
-    await driver.appMap.expandFindings();
-    await driver.appMap.openFindingsOverview();
-    const expectedFrames = 2;
-    await findingsOverviewWebview.initialize(expectedFrames);
-    await driver.appMap.findingsTreeItem(1).waitFor();
-    await findingsOverviewWebview.assertNumberOfFindingsInOverview(3);
-  });
-
-  it('opens the findings details page from the findings overview page', async function () {
-    const { driver, project } = this;
-
-    await project.restoreFiles('**/*.appmap.json');
-    await driver.waitForFile(path.join(project.workspacePath, 'tmp', '**', 'mtime')); // Wait for the indexer
-
-    const findingsOverviewWebview = driver.appMap['findingsOverviewWebview'];
-    const findingDetailsWebview = driver.appMap['findingDetailsWebview'];
-
-    await driver.appMap.openActionPanel();
-    await driver.appMap.expandFindings();
-    await driver.appMap.openFindingsOverview();
-
-    let expectedFrames = 2;
-    await findingsOverviewWebview.initialize(expectedFrames);
-    await findingsOverviewWebview.assertTitleRenders();
-    await driver.appMap.findingsTreeItem(1).waitFor();
-    await findingsOverviewWebview.openFirstFindingDetail();
-
-    expectedFrames = 3;
-    await findingDetailsWebview.initialize(expectedFrames);
-
-    const expectedTitle = 'N plus 1 SQL query';
-    await findingDetailsWebview.assertTitleRenders(expectedTitle);
-  });
 });
