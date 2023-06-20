2405fe3832c867ac69ff380937c12c54d13678b1
Kevin Gilpin <kgilpin@gmail.com>
2023-06-20 10:57:11 -0400

    feat: Show failed tests in the Analysis view

--- src/tree/findingsTreeDataProvider.ts
+++ src/tree/findingsTreeDataProvider.ts
@@ -5,6 +5,7 @@ import AppMapCollection from '../services/appmapCollection';
 import { ImpactDomain, Rule } from '@appland/scanner';
 import assert from 'assert';
 import { DATE_BUCKETS, DateBucket, ResolvedFinding } from '../services/resolvedFinding';
+import AppMapLoader from '../services/appmapLoader';
 
 const IMPACT_DOMAINS = ['Security', 'Performance', 'Stability', 'Maintainability'];
 
@@ -20,8 +21,41 @@ class WorkspaceFolderTreeItem extends vscode.TreeItem {
     super(folder.name, vscode.TreeItemCollapsibleState.Expanded);
   }
 
+  filterAppMaps(appmaps: AppMapLoader[]): AppMapLoader[] {
+    return appmaps.filter(
+      (appmap) =>
+        appmap.descriptor.metadata?.test_status === 'failed' &&
+        appmap.descriptor.resourceUri.fsPath.startsWith(this.folder.uri.fsPath)
+    );
+  }
+
   filterFindings(findings: ResolvedFinding[]): ResolvedFinding[] {
     return findings.filter((finding) => finding.folder === this.folder);
+  }
+}
+
+class FailedTestsTreeItem extends vscode.TreeItem {
+  constructor(public workspaceFolderTreeItem: WorkspaceFolderTreeItem) {
+    super('Failed tests', vscode.TreeItemCollapsibleState.Expanded);
+  }
+}
+
+class FindingsTreeItem extends vscode.TreeItem {
+  constructor(public workspaceFolderTreeItem: WorkspaceFolderTreeItem) {
+    super('Findings', vscode.TreeItemCollapsibleState.Expanded);
+  }
+}
+
+class FailedTestTreeItem extends vscode.TreeItem {
+  constructor(public failedTestsTreeItem: FailedTestsTreeItem, public appmap: AppMapLoader) {
+    super(appmap.descriptor.metadata?.name || '<unnamed appmap>');
+
+    this.command = {
+      title: 'Open',
+      command: 'vscode.openWith',
+      arguments: [this.appmap.descriptor.resourceUri, 'appmap.views.appMapFile'],
+    };
+    this.contextValue = 'appmap.views.analysis.failedTest';
   }
 }
 
@@ -75,6 +109,34 @@ class RuleTreeItem extends vscode.TreeItem {
   }
 }
 
+class FindingTreeItem extends vscode.TreeItem {
+  constructor(public ruleTreeItem: RuleTreeItem, public finding: ResolvedFinding) {
+    super(FindingTreeItem.label(finding));
+
+    const hashV2 = finding.finding.hash_v2;
+    this.command = {
+      command: 'appmap.openFindingInfo',
+      title: `Finding Info`,
+      arguments: [hashV2],
+    };
+
+    this.tooltip = finding.finding.message;
+    this.id = hashV2;
+    this.contextValue = 'appmap.views.analysis.finding';
+  }
+
+  static label(finding: ResolvedFinding): string {
+    const { event } = finding.finding;
+    const req = event['http_server_request'];
+    return (
+      finding.locationLabel ||
+      event.path ||
+      finding.finding.stack[0] ||
+      (req && `${req?.request_method} ${req?.path_info}`)
+    );
+  }
+}
+
 export class FindingsTreeDataProvider
   implements vscode.TreeDataProvider<vscode.TreeItem>, vscode.Disposable
 {
@@ -89,6 +151,8 @@ export class FindingsTreeDataProvider
       undefined,
       context.subscriptions
     );
+
+    appmaps.onUpdated(() => this._onDidChangeTreeData.fire(undefined));
   }
 
   public setFindingsIndex(findingsIndex?: FindingsIndex): void {
@@ -121,7 +185,7 @@ export class FindingsTreeDataProvider
 
     const projects = vscode.workspace.workspaceFolders;
 
-    const overviewTreeItem = new vscode.TreeItem('Overview');
+    const overviewTreeItem = new vscode.TreeItem('Findings table');
     overviewTreeItem.command = {
       command: 'appmap.openFindingsOverview',
       title: `Open in AppMap`,
@@ -138,8 +202,16 @@ export class FindingsTreeDataProvider
         return projectsWithFindings;
       }, new Set<vscode.WorkspaceFolder>());
 
+    const projectsWithAppMaps = this.appmaps.allAppMaps().reduce((projectsWithAppMaps, appmap) => {
+      const project = projects.find((project) =>
+        appmap.descriptor.resourceUri.fsPath.startsWith(project.uri.fsPath)
+      );
+      if (project) projectsWithAppMaps.add(project);
+      return projectsWithAppMaps;
+    }, new Set<vscode.WorkspaceFolder>());
+
     const projectTreeItems = projects
-      .filter((project) => projectsWithFindings.has(project))
+      .filter((project) => projectsWithFindings.has(project) || projectsWithAppMaps.has(project))
       .map((project) => new WorkspaceFolderTreeItem(project));
 
     return [overviewTreeItem, ...projectTreeItems];
@@ -154,6 +226,10 @@ export class FindingsTreeDataProvider
 
     if (element instanceof WorkspaceFolderTreeItem) {
       return this.getFolderTreeItems(element);
+    } else if (element instanceof FailedTestsTreeItem) {
+      return this.getFailedTestsTreeItems(element);
+    } else if (element instanceof FindingsTreeItem) {
+      return this.getFindingsTreeItems(element);
     } else if (element instanceof DateBucketTreeItem) {
       return this.getChildrenForDateBucket(element);
     } else if (element instanceof ImpactDomainTreeItem) {
@@ -166,12 +242,34 @@ export class FindingsTreeDataProvider
   }
 
   getFolderTreeItems(folderTreeItem: WorkspaceFolderTreeItem): vscode.TreeItem[] {
+    const result: vscode.TreeItem[] = [];
+    const appmaps = folderTreeItem.filterAppMaps(this.appmaps.allAppMaps());
+    if (appmaps.length > 0) result.push(new FailedTestsTreeItem(folderTreeItem));
+
+    if (this.findingsIndex) {
+      const findings = folderTreeItem.filterFindings(this.findingsIndex?.findings());
+      if (findings.length > 0) result.push(new FindingsTreeItem(folderTreeItem));
+    }
+
+    return result;
+  }
+
+  getFindingsTreeItems(findingsTreeItem: FindingsTreeItem): vscode.TreeItem[] {
     assert(this.findingsIndex);
-    const findings = folderTreeItem.filterFindings(this.findingsIndex?.findings());
+
+    const findings = findingsTreeItem.workspaceFolderTreeItem.filterFindings(
+      this.findingsIndex?.findings()
+    );
     const findingBuckets = new Set(findings.map((finding) => finding.dateBucket));
     return DATE_BUCKETS.filter((bucket) => findingBuckets.has(bucket)).map((bucket) => {
-      return new DateBucketTreeItem(folderTreeItem, bucket);
+      return new DateBucketTreeItem(findingsTreeItem.workspaceFolderTreeItem, bucket);
     });
+  }
+
+  getFailedTestsTreeItems(failedTestsTreeItem: FailedTestsTreeItem): vscode.TreeItem[] {
+    return failedTestsTreeItem.workspaceFolderTreeItem
+      .filterAppMaps(this.appmaps.allAppMaps())
+      .map((appmap) => new FailedTestTreeItem(failedTestsTreeItem, appmap));
   }
 
   getChildrenForDateBucket(dateBucketTreeItem: DateBucketTreeItem): vscode.TreeItem[] {
@@ -218,29 +316,7 @@ export class FindingsTreeDataProvider
       this.findingsIndex.distinctFindingsByRuleId(ruleTreeItem.rule.id)
     );
 
-    return findings.map((finding) => {
-      const hashV2 = finding.finding.hash_v2;
-      const { event } = finding.finding;
-      const req = event['http_server_request'];
-      const label =
-        finding.locationLabel ||
-        event.path ||
-        finding.finding.stack[0] ||
-        (req && `${req?.request_method} ${req?.path_info}`);
-
-      const treeItem = new vscode.TreeItem(String(label));
-
-      treeItem.command = {
-        command: 'appmap.openFindingInfo',
-        title: `Finding Info`,
-        arguments: [hashV2],
-      };
-
-      treeItem.tooltip = finding.finding.message;
-      treeItem.id = hashV2;
-
-      return treeItem;
-    });
+    return findings.map((finding) => new FindingTreeItem(ruleTreeItem, finding));
   }
 
   dispose(): void {
