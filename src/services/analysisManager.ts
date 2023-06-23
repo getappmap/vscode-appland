a8cbc5d56d979841de04bbd5964da8271e569a02
Rafał Rzepecki <divided.mind@gmail.com>
2023-06-23 10:54:18 +0200

    Actually remove FindingsWatcher when analysis is disabled

--- src/services/analysisManager.ts
+++ src/services/analysisManager.ts
@@ -138,6 +138,7 @@ export default class AnalysisManager {
     );
 
     this.workspaceServices.enroll(findingWatcher);
+    this.findingsWatcher = findingWatcher;
   }
 
   private static onAnalysisDisabled(): void {
