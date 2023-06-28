75844e6ce4bd78dede0de0abd78679e5fff0bf75
Adam Trotta <ahtrotta@gmail.com>
2023-06-28 14:36:45 -0400

    test: update vs code mocks for unit tests

---
+++ test/unit/mock/vscode/window.ts
@@ -0,0 +1,19 @@
+export default {
+  createStatusBarItem() {
+    return {
+      show() {
+        return '';
+      },
+    };
+  },
+  showInputBox() {
+    return '';
+  },
+  showErrorMessage() {
+    return;
+  },
+  showInformationMessage() {
+    return;
+  },
+  workspaceFolders: [],
+};
