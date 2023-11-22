cc13be498fed8dca34c9afd3d0463ee9f01f23b8
Kevin Gilpin <kgilpin@gmail.com>
2023-11-22 18:16:30 -0500

    feat: Provide free-form 'Search' command and add additional context

--- src/commands/search.ts
+++ src/commands/search.ts
@@ -1,15 +1,40 @@
 import * as vscode from 'vscode';
+import { SearchRpc } from '@appland/rpc';
+import { warn } from 'console';
+import { AppMapFilter, serializeFilter } from '@appland/models';
+import { FormatType, format, unparseDiagram } from '@appland/sequence-diagram';
+
 import IndexProcessWatcher from '../services/indexProcessWatcher';
 import { workspaceServices } from '../services/workspaceServices';
 import { NodeProcessService } from '../services/nodeProcessService';
 import { ProcessId } from '../services/processWatcher';
-import { warn } from 'console';
 import renderSearchResults from '../lib/renderSearchResults';
-import { AppMapFilter, serializeFilter } from '@appland/models';
-import { FormatType, format, unparseDiagram } from '@appland/sequence-diagram';
 import buildOpenAIApi from '../lib/buildOpenAIApi';
 import { explain } from '../lib/explain';
 import lookupSourceCode from './lookupSourceCode';
+
+async function promptForSearch(context: vscode.ExtensionContext) {
+  const query = await vscode.window.showInputBox({
+    placeHolder: 'Search AppMaps',
+    prompt: 'Enter a search query',
+  });
+  if (!query) return;
+
+  const workspaces = vscode.workspace.workspaceFolders;
+  if (!workspaces) return;
+
+  let workspaceFolder: vscode.WorkspaceFolder | undefined;
+  if (workspaces.length === 1) {
+    workspaceFolder = workspaces[0];
+  } else {
+    workspaceFolder = await vscode.window.showWorkspaceFolderPick({
+      placeHolder: 'Select a workspace folder',
+    });
+  }
+  if (!workspaceFolder) return;
+
+  return performSearch(context, query, workspaceFolder.uri);
+}
 
 async function performSearch(
   context: vscode.ExtensionContext,
@@ -90,42 +115,42 @@ async function performSearch(
 
   let explanation: string | undefined;
   {
-    const firstResult = searchResults.results[0];
-    const codeObjects = firstResult.events.map((event) => event.fqid);
-    const appmapFilter = new AppMapFilter();
-
-    appmapFilter.declutter.hideExternalPaths.on = true;
-    appmapFilter.declutter.context.on = true;
-    appmapFilter.declutter.context.names = codeObjects;
-
-    const filterState = serializeFilter(appmapFilter);
-    const sequenceDiagramData = await rpcClient.sequenceDiagram(
-      firstResult.appmap,
-      undefined,
-      filterState
-    );
-    const sequenceDiagram = unparseDiagram(sequenceDiagramData);
-    const plantUML = format(FormatType.PlantUML, sequenceDiagram, firstResult.appmap);
-
-    const doc = await vscode.workspace.openTextDocument({
-      language: 'plantuml',
-      content: plantUML.diagram,
-    });
-    await vscode.window.showTextDocument(doc, { preview: false });
-
+    const sequenceDiagrams = new Array<string>();
     const codeSnippets = new Map<string, string>();
-    for (const event of firstResult.events) {
-      if (!event.location) continue;
 
-      const snippet = await lookupSourceCode(workspaceUri.fsPath, event.location);
-      if (snippet) codeSnippets.set(event.location, snippet);
+    const buildSequenceDiagram = async (result: SearchRpc.SearchResult) => {
+      const codeObjects = result.events.map((event) => event.fqid);
+      const appmapFilter = new AppMapFilter();
+      appmapFilter.declutter.hideExternalPaths.on = true;
+      appmapFilter.declutter.context.on = true;
+      appmapFilter.declutter.context.names = codeObjects;
+      const filterState = serializeFilter(appmapFilter);
+
+      const sequenceDiagramData = await rpcClient.sequenceDiagram(
+        result.appmap,
+        undefined,
+        filterState
+      );
+      const sequenceDiagram = unparseDiagram(sequenceDiagramData);
+      const plantUML = format(FormatType.PlantUML, sequenceDiagram, result.appmap);
+      sequenceDiagrams.push(plantUML.diagram);
+    };
+
+    for (const result of searchResults.results.slice(0, 3)) {
+      await buildSequenceDiagram(result);
+      for (const event of result.events) {
+        if (!event.location) continue;
+
+        if (codeSnippets.has(event.location)) continue;
+
+        const snippet = await lookupSourceCode(workspaceUri.fsPath, event.location);
+        if (snippet) codeSnippets.set(event.location, snippet);
+      }
     }
 
     const openAI = await buildOpenAIApi(context);
     if (openAI) {
-      explanation = await explain(openAI, plantUML.diagram, codeSnippets);
-
-      (firstResult as any).explanation = explanation;
+      explanation = await explain(openAI, query, sequenceDiagrams, codeSnippets);
     }
   }
 
@@ -134,6 +159,7 @@ async function performSearch(
     workspace,
     query,
     tokenizedQuery,
+    explanation,
     searchResults
   );
   // Open as Markdown document
@@ -177,20 +203,22 @@ class QuickSearchProvider implements vscode.CodeActionProvider {
 }
 
 export function quickSearch(context: vscode.ExtensionContext) {
-  const provider = vscode.languages.registerCodeActionsProvider(
-    [
-      { language: 'ruby' },
-      { language: 'java' },
-      { language: 'python' },
-      { language: 'javascript' },
-    ],
-    new QuickSearchProvider()
+  context.subscriptions.push(
+    vscode.languages.registerCodeActionsProvider(
+      [
+        { language: 'ruby' },
+        { language: 'java' },
+        { language: 'python' },
+        { language: 'javascript' },
+      ],
+      new QuickSearchProvider()
+    )
   );
-  context.subscriptions.push(provider);
 
-  const command = vscode.commands.registerCommand(
-    'appmap.quickSearch',
-    performSearch.bind(null, context)
+  context.subscriptions.push(
+    vscode.commands.registerCommand('appmap.quickSearch', performSearch.bind(null, context))
   );
-  context.subscriptions.push(command);
+  context.subscriptions.push(
+    vscode.commands.registerCommand('appmap.search', promptForSearch.bind(null, context))
+  );
 }
