950b8a78774a0092440c2b8c7a282bacf8bc78b9
Adam Trotta <ahtrotta@gmail.com>
2023-11-29 15:23:22 -0500

    fixup! test: Drop specific vs code version number for testing

--- test/integrationTest.ts
+++ test/integrationTest.ts
@@ -106,21 +106,10 @@ async function integrationTest() {
     );
   }
 
-  cp.spawnSync(
-    cliPath,
-    [
-      '--extensions-dir',
-      extensionDevelopmentPath,
-      '--user-data-dir',
-      userDataDir,
-      '--force',
-      '--password-store=basic',
-    ],
-    {
-      encoding: 'utf-8',
-      stdio: 'inherit',
-    }
-  );
+  cp.spawnSync(cliPath, ['--user-data-dir', userDataDir, '--force', '--password-store=basic'], {
+    encoding: 'utf-8',
+    stdio: 'inherit',
+  });
 
   const runTests = async (testFile: string, workspaceDir: string) => {
     startTime = new Date();
