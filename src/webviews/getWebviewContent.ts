cebb5535d2f1452819b137e28e75efc7baf158c4
Kevin Gilpin <kgilpin@gmail.com>
2023-12-11 14:23:04 -0500

    revertme: Remove CSP from webview

--- src/webviews/getWebviewContent.ts
+++ src/webviews/getWebviewContent.ts
@@ -28,12 +28,6 @@ export default function getWebviewContent(
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="Content-Security-Policy" content="
-      default-src 'none';
-      img-src ${webview.cspSource} data:;
-      script-src ${webview.cspSource} 'unsafe-eval';
-      style-src ${webview.cspSource} 'unsafe-inline';
-    ">
     <link href="${cssUri}" rel="stylesheet">
 
     <title>${title}</title>
