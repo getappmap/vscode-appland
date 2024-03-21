89fabaa2ad4af9fa42cef05279801a9166c1ca7d
Dustin Byrne <dustinsbyrne@gmail.com>
2024-03-20 23:40:40 -0400

    test: Validate RPC service and config polling

--- test/integration/explain/explain.test.ts
+++ test/integration/explain/explain.test.ts
@@ -1,7 +1,6 @@
 import * as vscode from 'vscode';
 import { initializeWorkspace, waitFor, waitForExtension, withAuthenticatedUser } from '../util';
 import assert from 'assert';
-import { readyProcessWatchers } from '../../../src/lib/selectIndexProcess';
 import { initializeWorkspaceServices } from '../../../src/services/workspaceServices';
 
 describe('appmap.explain', () => {
@@ -18,11 +17,6 @@ describe('appmap.explain', () => {
 
     const workspace = vscode.workspace.workspaceFolders?.[0];
     assert(workspace);
-
-    await waitFor(
-      'Explain service is not ready',
-      () => readyProcessWatchers(workspace)?.length !== 0
-    );
 
     await waitFor('Invoking appmap.explain opens a new text document', async () => {
       await vscode.commands.executeCommand('appmap.explain');
