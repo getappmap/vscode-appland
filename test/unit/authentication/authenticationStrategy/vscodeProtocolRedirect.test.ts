227dd014941dcbfc4348ab8cef6f4cefbbe1498f
Rafał Rzepecki <divided.mind@gmail.com>
2024-07-10 20:16:50 +0200

    test: Add tests for vscodeProtocolRedirect

---
+++ test/unit/authentication/authenticationStrategy/vscodeProtocolRedirect.test.ts
@@ -0,0 +1,47 @@
+import '../../mock/vscode';
+
+import { expect } from 'chai';
+import sinon from 'sinon';
+
+import * as vscode from 'vscode';
+
+import VscodeProtocolRedirect from '../../../../src/authentication/authenticationStrategy/vscodeProtocolRedirect';
+import ExtensionSettings from '../../../../src/configuration/extensionSettings';
+import AppMapServerAuthenticationHandler from '../../../../src/uri/appmapServerAuthenticationHandler';
+import UriHandler from '../../../../src/uri/uriHandler';
+
+describe('VscodeProtocolRedirect', () => {
+  let uriHandler: UriHandler;
+  let authnHandler: AppMapServerAuthenticationHandler;
+  let vscodeProtocolRedirect: VscodeProtocolRedirect;
+
+  beforeEach(() => {
+    uriHandler = new UriHandler();
+    authnHandler = new AppMapServerAuthenticationHandler('your-nonce-value');
+    vscodeProtocolRedirect = new VscodeProtocolRedirect(uriHandler, authnHandler);
+    sinon
+      .stub(ExtensionSettings, 'appMapServerURL')
+      .get(() => vscode.Uri.parse('https://server.appmap.test'));
+  });
+
+  afterEach(() => {
+    vscodeProtocolRedirect.dispose();
+  });
+
+  it('should have the correct authnPath', () => {
+    expect(vscodeProtocolRedirect.authnPath).to.equal('authn_provider/vscode');
+  });
+
+  it('should redirect to the correct URL', async () => {
+    const queryParams = { client_id: '123' };
+    const redirectUrl = await vscodeProtocolRedirect.redirectUrl(Object.entries(queryParams));
+    expect(redirectUrl).to.equal('vscode://appland.appmap/authn-appmap-server?client_id%3D123');
+  });
+
+  it('should return the correct auth URL', () => {
+    const authUrl = vscodeProtocolRedirect.getAuthUrl();
+    expect(authUrl).to.deep.equal(
+      vscode.Uri.parse('https://server.appmap.test/authn_provider/vscode')
+    );
+  });
+});
--- test/unit/mock/vscode/env.ts
+++ test/unit/mock/vscode/env.ts
@@ -1,3 +1,9 @@
+import { Uri } from 'vscode';
+
 export function isTelemetryEnabled() {
   return false;
+}
+
+export async function asExternalUri(uri: Uri) {
+  return uri;
 }
