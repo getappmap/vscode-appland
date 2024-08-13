fe03fa881256e5cc6e597168f2d6c58e1ebd7707
Dustin Byrne <dustinsbyrne@gmail.com>
2024-08-12 22:01:03 -0400

    feat: New AppMap data recording instructions
    This change drops a lot of old behavior including language analysis,
    project setup milestones, system tests, java run configs.

--- src/webviews/chatSearchWebview.ts
+++ src/webviews/chatSearchWebview.ts
@@ -3,7 +3,6 @@ import getWebviewContent from './getWebviewContent';
 import appmapMessageHandler from './appmapMessageHandler';
 import FilterStore, { SavedFilter } from './filterStore';
 import WebviewList from './WebviewList';
-import { ProjectPicker, RecordAppMaps } from '../tree/instructionsTreeDataProvider';
 import { getApiKey } from '../authentication';
 import ExtensionSettings from '../configuration/extensionSettings';
 import { CodeSelection } from '../commands/quickSearch';
@@ -136,10 +135,8 @@ export default class ChatSearchWebview {
           void vscode.commands.executeCommand('appmap.explain');
           break;
         case 'open-record-instructions':
-          await vscode.commands.executeCommand('appmap.openInstallGuide', RecordAppMaps);
-          break;
         case 'open-install-instructions':
-          await vscode.commands.executeCommand('appmap.openInstallGuide', ProjectPicker);
+          await vscode.commands.executeCommand('appmap.openInstallGuide');
           break;
         case 'open-appmap': {
           const uri = vscode.Uri.file(message.path);
--- src/workspace/projectMetadata.ts
+++ src/workspace/projectMetadata.ts
@@ -1,12 +1,6 @@
-import { SUPPORTED_LANGUAGES } from '../services/languageResolver';
-import { RunConfigStatus } from '../services/runConfigService';
-import Feature from './feature';
-
 export default interface ProjectMetadata {
   name: string;
   path: string;
-  score?: number;
-  hasNode?: boolean;
   agentInstalled?: boolean;
   appMapsRecorded?: boolean;
   openedNavie?: boolean;
@@ -15,34 +9,4 @@ export default interface ProjectMetadata {
   numFindings?: number;
   numHttpRequests?: number;
   numAppMaps?: number;
-  // Most preferred language available.
-  language?: Feature;
-  // All recognized languages.
-  languages?: Feature[];
-  // Test framework recognized in the preferred language, if any.
-  testFramework?: Feature;
-  // Web framework recognized in the preferred language, if any.
-  webFramework?: Feature;
-  debugConfigurationStatus?: RunConfigStatus;
-}
-
-export function isLanguageSupported(project?: ProjectMetadata): boolean {
-  if (!project) return false;
-
-  return !!project.languages?.some(
-    (language) =>
-      language.name &&
-      SUPPORTED_LANGUAGES.includes(
-        language.name.toLowerCase() as (typeof SUPPORTED_LANGUAGES)[number]
-      )
-  );
-}
-
-export function hasSupportedFramework(project?: ProjectMetadata): boolean {
-  if (!project) return false;
-
-  return !!(
-    (project.webFramework && project.webFramework.score > 0) ||
-    (project.testFramework && project.testFramework.score > 0)
-  );
 }
