bc089c577a3c848c8ba4f88a9bc9d2598053c5bd
Dustin Byrne <dustinsbyrne@gmail.com>
2024-11-12 13:10:25 -0500

    test: Run sign in tests as unit tests

--- test/unit/sidebarSignIn.test.ts
+++ test/unit/sidebarSignIn.test.ts
@@ -1,15 +1,15 @@
-import * as vscode from 'vscode';
 import assert from 'assert';
 import sinon from 'sinon';
-import * as auth from '../../../src/authentication';
-import SignInManager from '../../../src/services/signInManager';
-import MockExtensionContext from '../../mocks/mockExtensionContext';
-import ExtensionState from '../../../src/configuration/extensionState';
+import vscode from './mock/vscode';
+
+import * as auth from '../../src/authentication';
+import SignInManager from '../../src/services/signInManager';
+import MockExtensionContext from '../mocks/mockExtensionContext';
+import ExtensionState from '../../src/configuration/extensionState';
 
 describe('Sidebar sign-in', () => {
   let sandbox: sinon.SinonSandbox;
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  let stubbedExecuteCommand: sinon.SinonStub<[command: string, ...rest: any[]], Thenable<unknown>>;
+  let stubbedExecuteCommand;
   const context = new MockExtensionContext();
   const extensionState = new ExtensionState(context);
   const existingUserVersion = '0.66.2';
@@ -43,7 +43,7 @@ describe('Sidebar sign-in', () => {
     assert(!SignInManager.shouldShowSignIn());
 
     expectedArgs = ['setContext', 'appMap.showSignIn', false];
-    assert.deepStrictEqual(stubbedExecuteCommand.getCall(2).args, expectedArgs);
+    assert.deepStrictEqual(stubbedExecuteCommand.getCall(1).args, expectedArgs);
   });
 
   it('is not shown for a new user who is authenticated, but is shown when they log out', async () => {
@@ -63,7 +63,7 @@ describe('Sidebar sign-in', () => {
     assert(SignInManager.shouldShowSignIn());
 
     expectedArgs = ['setContext', 'appMap.showSignIn', true];
-    assert.deepStrictEqual(stubbedExecuteCommand.getCall(2).args, expectedArgs);
+    assert.deepStrictEqual(stubbedExecuteCommand.getCall(1).args, expectedArgs);
   });
 
   it('is not shown for an existing user who is not authenticated and then logs in', async () => {
@@ -83,7 +83,7 @@ describe('Sidebar sign-in', () => {
     assert(!SignInManager.shouldShowSignIn());
 
     expectedArgs = ['setContext', 'appMap.showSignIn', false];
-    assert.deepStrictEqual(stubbedExecuteCommand.getCall(2).args, expectedArgs);
+    assert.deepStrictEqual(stubbedExecuteCommand.getCall(1).args, expectedArgs);
   });
 
   it('is shown for a new user who is not authenticated, but is not shown once they log in', async () => {
