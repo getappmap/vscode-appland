5a6e6570023900633e78f409e96882c72cbb2748
Rafał Rzepecki <divided.mind@gmail.com>
2025-09-22 16:01:34 +0200

    fixup! fix: update appmap and scanner versions, and correct jar filename in download function

--- build/updateResources.js
+++ build/updateResources.js
@@ -40,7 +40,7 @@ async function main() {
   const appmapVersion = await fetchLatestNpmVersion('@appland/appmap');
   const scannerVersion = await fetchLatestNpmVersion('@appland/scanner');
   const agentJarVersion = await fetchLatestGitHubReleaseVersion('getappmap/appmap-java');
-  const jarExists = await fileExists(join(resourceDir, 'appmap-java.jar'));
+  const jarExists = await fileExists(join(resourceDir, `appmap-${agentJarVersion}.jar`));
   const versionDeclarations = await readFile(join(resourceDir, 'versions.json'), 'utf-8').then(
     JSON.parse
   );
